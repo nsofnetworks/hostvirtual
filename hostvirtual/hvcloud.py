a334c6b7fbc71c1db65351d64aa13ea35b3c7b53
Shmulik Ladkani <shmulik@nsof.io>
2017-07-05 22:53:44 +0300

    Introduce HVCloud: Client for HostVirtual /cloud API
    Currently supported:
     - Issue a generic API request
     - List packages and available (unassigned) packages
     - List locations
     - Convert location code to ID
     - List servers
     - Wait for a server condition
     - Modify server parameters

---
+++ hostvirtual/hvcloud.py
@@ -0,0 +1,118 @@
+'''Python module for the HostVirtual Cloud API'''
+import os
+import time
+import requests
+
+
+class HVException(RuntimeError):
+    '''HostVirtual Custom Exception'''
+    pass
+
+
+class HVCloud(object):
+    '''Client for HostVirtual Cloud API'''
+
+    HV_API_ENDPOINT = 'http://bapi.vr.org'
+
+    def __init__(self, api_key):
+        self._key = api_key
+        if self._key is None:
+            self._key = os.environ.get('HV_API_KEY', '')
+        self._locations = None  # lazy evaluated, cached
+        self._packages = None  # lazy evaluated, cached
+
+    @staticmethod
+    def _request(op, url, in_query, params):
+        kwargs = dict(url=url, timeout=5)
+        kwargs['params' if in_query else 'json'] = params
+        return requests.request(op, **kwargs)
+
+    @staticmethod
+    def _sanitize(params):
+        for k in list(params):
+            if params[k] is None:
+                del params[k]
+
+    def request(self, op, ep, in_query=False, **params):
+        '''Issue an API request'''
+        self._sanitize(params)
+        url = "%s%s?key=%s" % (self.HV_API_ENDPOINT, ep, self._key)
+        resp = self._request(op, url, in_query, params)
+        if not resp.ok:
+            try:
+                err = resp.json()['error']['message']
+            except:
+                err = ''
+            msg = 'Failed %s %s - (%s) %s' % (op, ep, resp.status_code, err)
+            raise HVException(msg)
+        return resp.json()
+
+    @property
+    def packages(self):
+        '''List all packages'''
+        # lazy evaluation; cached during self's lifetime
+        if not self._packages:
+            self._packages = self.request('GET', '/cloud/packages')
+        return self._packages
+
+    def available_packages(self):
+        '''List unassigned packages'''
+        def _is_available(pkg):
+            return (pkg.get('package_status') == 'Active' and
+                    pkg.get('package') is None and
+                    pkg.get('state') is None)
+        return (p for p in self.packages if _is_available(p))
+
+    @property
+    def locations(self):
+        '''List all locations'''
+        # lazy evaluation; cached during self's lifetime
+        if not self._locations:
+            self._locations = {}
+            ret = self.request('GET', '/cloud/locations')
+            for k, loc in ret.iteritems():
+                code = k.split(' - ', 1)[0].upper()
+                self._locations[code] = loc
+        return self._locations
+
+    def location_id(self, code):
+        '''Return location ID given its letter code'''
+        code = code.upper()
+        try:
+            loc_id = self.locations[code]['id']
+        except KeyError:
+            raise HVException('No such location %s' % (code,))
+        return loc_id
+
+    def servers(self):
+        '''List all servers'''
+        return self.request('GET', '/cloud/servers')
+
+    def _server_test_condition(self, mbpkgid, condition_fn):
+        try:
+            srv = self.request('GET', '/cloud/server/%s' % (mbpkgid,))
+        except HVException:
+            srv = {}
+        return srv if condition_fn(srv) else None
+
+    def server_wait_for(self, mbpkgid, condition_fn):
+        '''Wait for server to match a condition'''
+        for _ in xrange(60):
+            srv = self._server_test_condition(mbpkgid, condition_fn)
+            if srv:
+                return srv
+            time.sleep(5)
+        msg = 'Timed out waiting on server (mbpkgid=%s)' % (mbpkgid,)
+        raise HVException(msg)
+
+    def server_modify(self, mbpkgid, **params):
+        '''Modify server parameters'''
+        srv = self.request('GET', '/cloud/server/%s' % (mbpkgid,))
+        for p, v in params.iteritems():
+            if srv.get(p) != v:
+                break
+        else:
+            return srv  # nothing to modify
+
+        srv.update(params)
+        return self.request('PUT', '/cloud/server/%s' % (mbpkgid,), **srv)
